7726c2c0e69a3eb5f46caa55a8cda6090c64d1b8
Nawaraj Pudasaini <active99raj@gmail.com>
2025-07-28 19:00:16 +1000

    Handling CORS Errors

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const morgan = require('morgan');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// prevent CORS error for API requests to server
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+});
+
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders');
 
