9976c64fee970e25e2d252476a19c8b40e6e8d99
Nawaraj Pudasaini <active99raj@gmail.com>
2025-07-28 13:23:41 +1000

    Added patch and delete products and all rest api endpoints on orders

---
+++ api/routes/orders.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Orders to /orders being fetched"
+    })
+})
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: "Orders to /orders being created"
+    })
+})
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details',
+        orderId: req.params.orderId
+    })
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order deleted !',
+        orderId: req.params.orderId
+    })
+})
+module.exports = router;
